43db1134fa48452ba93f57a08be96ee2ebd81e95
Cd4840 <130537470+Cd4840@users.noreply.github.com>
2026-03-04 20:26:55 -0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
